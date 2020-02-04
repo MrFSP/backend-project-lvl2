ea576753a162929fd86348964e363eea57d22375
Pavel Egorov MrFSP <epe.m@mail.ru>
2020-02-04 13:46:37 +0300

    step 7

--- Makefile
+++ Makefile
@@ -1,8 +1,14 @@
 install:
 	npm install
 
-start:
-	npx babel-node src/bin/gendiff.js -V
+start1:
+	npx babel-node src/bin/gendiff.js __tests__/fixtures/before.json __tests__/fixtures/after.json
+
+start2:
+	npx babel-node src/bin/gendiff.js __tests__/fixtures/before.yml __tests__/fixtures/after.yml
+
+start3:
+	npx babel-node src/bin/gendiff.js __tests__/fixtures/before.ini __tests__/fixtures/after.ini
 
 publish:
 	--dry-run
--- src/index.js
+++ src/index.js
@@ -1,34 +1,75 @@
 import _ from 'lodash';
 import parse from './parsers';
 
+const isObject = (data) => typeof (data) === 'object';
 
-const getKeys = (data1, data2) => [...Object.keys(data1), ...Object.keys(data2)]
-  .reduce((acc, key) => (acc.includes(key) ? acc : [...acc, key]), []);
+const getDifference = (data1, data2) => {
+  const keys = _.union(Object.keys(data1), Object.keys(data2)).sort();
+  return keys.map((key) => {
+    if (isObject(data1[key]) && isObject(data2[key])) {
+      return { key, type: 'objects', children: getDifference(data1[key], data2[key]) };
+    }
+    if (data1[key] === data2[key]) {
+      return {
+        key, type: 'equal', oldValue: data1[key], newValue: data2[key],
+      };
+    }
+    if (!_.has(data1, key)) {
+      return { key, type: 'added', newValue: data2[key] };
+    }
+    if (!_.has(data2, key)) {
+      return { key, type: 'removed', newValue: data1[key] };
+    }
+    if (_.has(data1, key) && _.has(data2, key) && data1[key] !== data2[key]) {
+      return {
+        key, type: 'changed', oldValue: data1[key], newValue: data2[key],
+      };
+    }
+    return null;
+  }).sort();
+};
 
-const getDifference = (keys, data1, data2) => keys.reduce((acc, key) => {
-  if (data1[key] === data2[key]) {
-    const newItem = `    ${key}: ${data2[key]}`;
-    return [...acc, newItem];
+const stringify = (data, key, lvl = 0, change = ' ', obj = false) => {
+  if (isObject(data) || obj === true) {
+    const newData = obj === true ? data : Object.keys(data)
+      .map((k) => stringify(data[k], k, lvl + 4))
+      .sort();
+    return `\n${' '.repeat(lvl)}${change} ${key}: {${newData}\n${' '.repeat(lvl + 2)}}`;
   }
-  if (_.has(data1, key) && !_.has(data2, key)) {
-    const newItem = `  - ${key}: ${data1[key]}`;
-    return [...acc, newItem];
+  return `\n${' '.repeat(lvl)}${change} ${key}: ${data}`;
+};
+
+const getRendering = (data, lvl) => data.reduce((newAcc, item) => {
+  const currentLvl = lvl + 2;
+  if (item.type === 'objects') {
+    const newData = getRendering(item.children, currentLvl + 2);
+    const newItem = stringify(newData, item.key, currentLvl, ' ', true);
+    return [...newAcc, newItem];
   }
-  if (!_.has(data1, key) && _.has(data2, key)) {
-    const newItem = `  + ${key}: ${data2[key]}`;
-    return [...acc, newItem];
+  if (item.type === 'equal') {
+    const newItem = stringify(item.newValue, item.key, currentLvl);
+    return [...newAcc, newItem];
   }
-  const newItem1 = `  + ${key}: ${data2[key]}`;
-  const newItem2 = `  - ${key}: ${data1[key]}`;
-  return [...acc, newItem1, newItem2];
-}, []);
+  if (item.type === 'added') {
+    const newItem = stringify(item.newValue, item.key, currentLvl, '+');
+    return [...newAcc, newItem];
+  }
+  if (item.type === 'removed') {
+    const newItem = stringify(item.newValue, item.key, currentLvl, '-');
+    return [...newAcc, newItem];
+  }
+  if (item.type === 'changed') {
+    const oldItem = stringify(item.oldValue, item.key, currentLvl, '-');
+    const newItem = stringify(item.newValue, item.key, currentLvl, '+');
+    return [...newAcc, oldItem, newItem];
+  }
+  return [...newAcc];
+}, []).join('');
 
 export default (firstConfig, secondConfig) => {
-  const config1 = parse(firstConfig);
-  // console.log(config1);
-  const config2 = parse(secondConfig);
-  // console.log(config2);
-  const keysOfConfigFiles = getKeys(config1, config2);
-  const difference = getDifference(keysOfConfigFiles, config1, config2);
-  return `{\n${difference.join('\n')}\n}`;
+  const data1 = parse(firstConfig);
+  const data2 = parse(secondConfig);
+  const difference = getDifference(data1, data2);
+  const rendering = `{${getRendering(difference, 0)}\n}`;
+  return rendering;
 };
