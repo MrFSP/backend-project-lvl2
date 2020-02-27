30bfd02698a9829a7a61dc3d02c1c8883207b45b
Pavel Egorov MrFSP <epe.m@mail.ru>
2020-02-27 22:53:58 +0300

    refactor

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,16 +4,12 @@ import program from 'commander';
 import { version, description } from '../../package.json';
 import genDiff from '..';
 
-try {
-  program
-    .description(description)
-    .version(`gendiff version: ${version}`, '-v, --version', 'output the version number')
-    .option('-f, --format [type]',
-      'Choose output format.\n\t\t       Types: simple, plain, json.\n\t\t      ', 'simple')
-    .arguments('<firstConfig> <secondConfig>')
-    .action((firstConfig, secondConfig, option) => console.log(genDiff(firstConfig,
-      secondConfig, option.format)))
-    .parse(process.argv);
-} catch (error) {
-  console.log(error);
-}
+program
+  .description(description)
+  .version(`gendiff version: ${version}`, '-v, --version', 'output the version number')
+  .option('-f, --format [type]',
+    'Choose output format.\n\t\t       Types: simple, plain, json.\n\t\t      ', 'simple')
+  .arguments('<firstConfig> <secondConfig>')
+  .action((firstConfig, secondConfig, option) => console.log(genDiff(firstConfig,
+    secondConfig, option.format)))
+  .parse(process.argv);
